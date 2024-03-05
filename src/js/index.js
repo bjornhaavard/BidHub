3f5eff6a407df3ed3ee3e77d78f43e88e8fc8dc5
bjornhaavard <bjornhaavard@hotmail.com>
2024-03-05 16:33:15 +0100

    added eslint and lint scripts

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,26 @@
 // Define the router function
 function router() {
-    const path = window.location.pathname;
+  const path = window.location.pathname;
 
   switch (path) {
     case "/":
     case "/index.html":
       // Handle the root path
-      console.log('Welcome to the homepage');
+      console.log("Welcome to the homepage");
       break;
-    case '/about':
+    case "/about":
       // Handle the about path
-      console.log('This is the about page');
+      console.log("This is the about page");
       break;
-    case '/contact':
+    case "/contact":
       // Handle the contact path
-      console.log('Contact us at example@example.com');
+      console.log("Contact us at example@example.com");
       break;
     default:
       // Handle any other paths
-      console.log('Page not found');
+      console.log("Page not found");
       break;
   }
 }
 // Calling the router function
 router();
-
