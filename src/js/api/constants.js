708418c3698c0f418af5843a2a51d5775903b237
bjornhaavard <bjornhaavard@hotmail.com>
2024-04-02 21:08:05 +0200

    this is just console logging constants

--- src/js/api/constants.js
+++ src/js/api/constants.js
@@ -1,5 +1,6 @@
 export const API_HOST_URL = "https://v2.api.noroff.dev";
 export const API_LISTINGS = "/auction/listings";
+export const API_PROFILES = "/auction/profiles";
 
-console.log(`${API_HOST_URL}${API_LISTINGS} `);
+console.log(`${API_HOST_URL}${API_PROFILES} `);
 
