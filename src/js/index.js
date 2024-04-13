43ef850994cfeea636acddc561a048a32e680292
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2024-04-13 21:00:31 +0200

    paths

--- src/js/index.js
+++ src/js/index.js
@@ -1,39 +1,39 @@
 // Define the router function
 
 import { setRegisterFormListener } from "../js/handlers/register.js";
-import { setLoginFormListener } from "../handlers/login.js";
-import { displayListings } from "../handlers/displayListing.js";
+import { setLoginFormListener } from "../../src/js/handlers/login.js";
+import { displayListings } from "../js/handlers/displayListing.js";
 
 function router() {
   const path = window.location.pathname;
 
   switch (path) {
-    case "../":
-    case "../index.html":
+    case "./":
+    case "./index.html":
       // Handle the root path
       displayListings();
 
       break;
 
-    case "../profile/":
-    case "../profile/index.html":
+    case "./profile/":
+    case "./profile/index.html":
       // Handle the profile path
 
       break;
 
-    case "../auth/login/":
-    case "../auth/login/index.html":
+    case "./auth/login/":
+    case "./auth/login/index.html":
       setLoginFormListener();
 
       break;
 
-    case "../auth/register/":
-    case "../auth/register/index.html":
+    case "./auth/register/":
+    case "./auth/register/index.html":
       setRegisterFormListener();
 
       break;
 
-    case "../contact":
+    case "./contact":
       // Handle the contact path
       console.log("Contact us at example@example.com");
       break;
