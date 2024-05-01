4f67f46767fbc0a5a0d57725478dc2320ea7a528
bjornhaavard <94046432+bjornhaavard@users.noreply.github.com>
2024-05-01 15:09:08 +0200

    change avatar image path

--- src/js/handlers/updateProfileImage.js
+++ src/js/handlers/updateProfileImage.js
@@ -26,7 +26,7 @@ export function updateProfile() {
         console.log(error);
       }
 
-      window.location.pathname = "/BidHub/auth/profile/";
+      window.location.pathname = "../auth/profile/";
     });
   }
 }
